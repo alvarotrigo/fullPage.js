3f15078633833316143f482c1b774c0a0caefc45
Kyler Berry <kyler@kylerberry.com>
2014-04-17 12:14:59 -0400

    - changed scrollspeed back

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -59,7 +59,7 @@
 		//Defines the delay to take place before being able to scroll to the next section
 		//BE CAREFUL! Not recommened to change it under 400 for a good behavior in laptops and
 		//Apple devices (laptops, mouses...)
-		var scrollDelay = 500;
+		var scrollDelay = 600;
 
 		$.fn.fullpage.setAutoScrolling = function(value){
 			options.autoScrolling = value;
