8189f1a79b0e4676420f148fb2a03cee86945288
Will Xu <2bethere@gmail.com>
2014-07-27 14:10:03 -0700

    Updating the pull request to fix feedbacks

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -39,7 +39,7 @@
 			'continuousVertical': false,
 			'animateAnchor': true,
 			'normalScrollElementTouchThreshold': 5,
-            'limitToDiv': false,
+			'partialPage': false,
 
 			//events
 			'afterLoad': null,
@@ -135,8 +135,8 @@
 
 		var isTablet = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|Windows Phone|Tizen|Bada)/);
 		var container = $(this); // for compatibity reasons for fullpage < v2.0
-        var windowsHeight = options.limitToDiv?$(this).height():$(window).height();
-        var windowsWidth = options.limitToDiv?$(this).width():$(window).width();
+		var windowsHeight = options.partialPage?$(this).height():$(window).height();
+		var windowsWidth = options.partialPage?$(this).width():$(window).width();
 		var isMoving = false;
 		var isResizing = false;
 		var lastScrolledDestiny;
@@ -149,15 +149,15 @@
 			options.css3 = support3d();
 		}
 
-        // Wrap the container in a mask to hide overflown sections when targeting a div
-        if(options.limitToDiv){
-            container.wrap('<div class="fullPage-mask"></div>');
-            var maskcontainer = container.parent();
-            maskcontainer.css({
-                'height': windowsHeight,
-                'width': windowsWidth
-            });
-        }
+		// Wrap the container in a mask to hide overflown sections when targeting a div
+		if(options.partialPage){
+		    container.wrap('<div class="fullPage-mask"></div>');
+		    var maskcontainer = container.parent();
+		    maskcontainer.css({
+			'height': windowsHeight,
+			'width': windowsWidth
+		    });
+		}
 
 		if($(this).length){
 			container.css({
@@ -1039,17 +1039,14 @@
 		 */
 		function doneResizing() {
 			isResizing = true;
-
-            if(!options.limitToDiv)
-            {
-                var windowsWidth = $(window).width();
-                windowsHeight = $(window).height();
-            }else{
-                var windowsWidth = $(this).width();
-                windowsHeight = $(this).height();
-            }
-
-            //text and images resizing
+			if(!options.partialPage){
+			    var windowsWidth = $(window).width();
+			    windowsHeight = $(window).height();
+			}else{
+			    var windowsWidth = $(this).width();
+			    windowsHeight = $(this).height();
+			}
+			//text and images resizing
 			if (options.resize) {
 				resizeMe(windowsHeight, windowsWidth);
 			}
