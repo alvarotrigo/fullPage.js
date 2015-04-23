5d7e0da8733fa512a9fbb771919530344bdc4567
sato <sato@tsumikiinc.com>
2015-04-23 18:27:46 +0900

    edit params

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -5,16 +5,18 @@
  *
  * Copyright (C) 2015 alvarotrigo.com - A project by Alvaro Trigo
  */
-(function(factory) {
+(function(global, factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
+        define(['jquery'], function($) {
+          return factory($, global, global.document, global.Math);
+        });
     } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
+        module.exports = factory(require('jquery'), global, global.document, global.Math);
     } else {
-        factory(jQuery);
+        factory(jQuery, global, global.document, global.Math);
     }
-})(function($) {
+})(typeof window !== 'undefined' ? window : this, function($, window, document, Math, undefined) {
     'use strict';
 
     // keeping central set of classnames and selectors
