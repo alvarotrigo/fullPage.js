bbfd70c34fb2f80699ec0d65eab7cfd167102ceb
Cem Derin <hello@cemderin.net>
2015-06-03 00:12:19 +0200

    - reponsive parameter can be a function which returns true or false

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -1577,15 +1577,24 @@
         */
         function responsive(){
             if(options.responsive){
-                var isResponsive = container.hasClass(RESPONSIVE);
-                if ($window.width() < options.responsive ){
+                var isResponsive = container.hasClass(RESPONSIVE),
+                    setResponsive = false;
+
+                // is responsive option a function
+                if(typeof(options.responsive) === "function") {
+                    setResponsive = options.responsive();
+                } else if ($window.width() < options.responsive ){
+                    setResponsive = true;
+                }
+
+                if(setResponsive){
                     if(!isResponsive){
                         FP.setAutoScrolling(false, 'internal');
                         FP.setFitToSection(false, 'internal');
                         $(SECTION_NAV_SEL).hide();
                         container.addClass(RESPONSIVE);
                     }
-                }else if(isResponsive){
+                } else if(isResponsive){
                     FP.setAutoScrolling(originals.autoScrolling, 'internal');
                     FP.setFitToSection(originals.autoScrolling, 'internal');
                     $(SECTION_NAV_SEL).show();
