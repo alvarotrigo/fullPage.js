ee8ecfa80447fb2276a65a586cd78484fb00ff77
Gustav Stensson <st3nsson@gmail.com>
2016-11-03 09:50:49 +0100

    Use locally injected variable $
    I got error since I didn't have jQuery globally defined. All other places in the code `$` is used as well, not sure why `jQuery` was used here

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -2954,7 +2954,7 @@
 
             scrollable.height(scrollHeight);
             scrollable.each(function() {
-                var $this = jQuery(this);
+                var $this = $(this);
                 var iScrollInstance = $this.data('iscrollInstance');
                 if (iScrollInstance) {
                     $.each(iscrollHandler.iScrollInstances, function(){
