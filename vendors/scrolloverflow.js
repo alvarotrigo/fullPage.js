9fd1e70ef95398b2ae3df1108df09362b26cc119
Alvaro Trigo López <atrigol@gmail.com>
2017-10-27 15:57:06 +0100

    - Fixed bug: scroll overflow didn’t work when using jQuery.noConflit() #2962

--- vendors/scrolloverflow.js
+++ vendors/scrolloverflow.js
@@ -1,5 +1,5 @@
 /*!
-* Customized version of iScroll.js 0.0.2
+* Customized version of iScroll.js 0.0.3
 * It fixes bugs affecting its integration with fullpage.js
 */
 /*! iScroll v5.2.0 ~ (c) 2008-2016 Matteo Spinelli ~ http://cubiq.org/license */
