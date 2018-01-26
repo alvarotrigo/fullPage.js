59319ba63487af12998a12fb59ba3d5f81aaead4
Alvaro Trigo López <atrigol@gmail.com>
2018-01-26 17:56:19 +0100

    - Fixed bug: swipe horizontally in Firefox mobile not working when using autoScrolling:false #2741

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -2619,8 +2619,8 @@
             events.y = (typeof e.pageY !== 'undefined' && (e.pageY || e.pageX) ? e.pageY : e.touches[0].pageY);
             events.x = (typeof e.pageX !== 'undefined' && (e.pageY || e.pageX) ? e.pageX : e.touches[0].pageX);
 
-            //in touch devices with scrollBar:true, e.pageY is detected, but we have to deal with touch events. #1008
-            if(isTouch && isReallyTouch(e) && options.scrollBar){
+            //in touch devices with scroll bar, e.pageY is detected, but we have to deal with touch events. #1008
+            if(isTouch && isReallyTouch(e) && (options.scrollBar || !options.autoScrolling)){
                 events.y = e.touches[0].pageY;
                 events.x = e.touches[0].pageX;
             }
