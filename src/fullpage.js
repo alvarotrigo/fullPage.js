5bf712ddeb71f2d0b8c8c853a94c020603c26e81
Ethan Strauss <estrauss2018@gmail.com>
2018-09-12 11:31:26 -0400

    Add scroll check in mousewheelhandler
    Added a quick check to see if scrolling is allowed in the MouseWheelHandler function. If not, prevent default behaviour and return.

--- src/fullpage.js
+++ src/fullpage.js
@@ -1328,6 +1328,12 @@
         function MouseWheelHandler(e) {
             var curTime = new Date().getTime();
             var isNormalScroll = hasClass($(COMPLETELY_SEL)[0], NORMAL_SCROLL);
+            
+            //is scroll allowed?
+            if (!isScrollAllowed.m.down && !isScrollAllowed.m.up) {
+                preventDefault(e);
+                return false;
+            }
 
             //autoscrolling and not zooming?
             if(options.autoScrolling && !controlPressed && !isNormalScroll){
