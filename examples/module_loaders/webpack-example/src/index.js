40282eb092f2a3210bacc489d1a4387b82db2614
Alvaro Trigo López <atrigol@gmail.com>
2018-09-25 16:03:07 +0200

    - Updating module loaders examples (extensions & scrollOverflow)

--- examples/module_loaders/webpack-example/src/index.js
+++ examples/module_loaders/webpack-example/src/index.js
@@ -1,10 +1,20 @@
+// Optional. When using fullPage extensions
+//import scrollHorizontally from './fullpage.scrollHorizontally.min';
+
+// Optional. When using scrollOverflow:true
+//import IScroll from 'fullpage.js/vendors/scrolloverflow';
+
 // Importing fullpage.js
 import fullpage from 'fullpage.js';
+
+// When using fullPage extensions replace the previous import
+// of fullpage.js for this file
+//import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
 
 // Initializing it
 var fullPageInstance = new fullpage('#myFullpage', {
     navigation: true,
-    sectionsColor:['blue', 'red', 'purple', 'grey']
+    sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
 });
 
 // Calling methods
