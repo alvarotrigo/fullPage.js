7a0f623f0930241483f8507bb2113a3978f7ea1e
Alvaro Trigo López <atrigol@gmail.com>
2022-09-08 13:14:27 +0200

    - Fixed bug: licenseKey was showing an error on IE 11

--- dist/fullpage.js
+++ dist/fullpage.js
@@ -215,6 +215,31 @@
       });
     }
 
+    // https://stackoverflow.com/questions/51719553/padstart-not-working-in-ie11
+    // https://github.com/behnammodi/polyfill/blob/master/string.polyfill.js
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+    if (!String.prototype.padStart) {
+      String.prototype.padStart = function padStart(targetLength, padString) {
+        targetLength = targetLength >> 0; //truncate if number or convert non-number to 0;
+
+        padString = String(typeof padString !== 'undefined' ? padString : ' ');
+
+        if (this.length > targetLength) {
+          return String(this);
+        } else {
+          targetLength = targetLength - this.length;
+
+          if (targetLength > padString.length) {
+            padString += Array.apply(null, Array(targetLength)).map(function () {
+              return padString;
+            }).join("");
+          }
+
+          return padString.slice(0, targetLength) + String(this);
+        }
+      };
+    }
+
     //utils
     /**
     * Shows a message in the console of the given type.
@@ -5344,7 +5369,7 @@
         });
       });
       var t = ["-"];
-      var n = "2022-8-7".split("-"),
+      var n = "2022-8-8".split("-"),
           e = new Date(n[0], n[1], n[2]),
           i = ["se", "licen", "-", "v3", "l", "gp"];
 
