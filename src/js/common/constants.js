b79edfd3e622e7c16cd96c1809656c07eaae1d92
Jay Collett <hello@jaycollett.co>
2023-06-13 15:24:18 +0100

    Update constants.js
    Adding the summary HTML element to the focusable elements constant.
    
    Building an application using the Gov.uk Design System and found a bug where the Details component wasn't focussable.
    
    https://design-system.service.gov.uk/components/details/

--- src/js/common/constants.js
+++ src/js/common/constants.js
@@ -8,7 +8,7 @@ export const isTouch = (('ontouchstart' in win) || (navigator.msMaxTouchPoints >
 export const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 
 // taken from https://github.com/udacity/ud891/blob/gh-pages/lesson2-focus/07-modals-and-keyboard-traps/solution/modal.js
-export const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+export const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], summary:not([disabled]), [contenteditable]';
 
 // cache common elements
 export const FP = {
